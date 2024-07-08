905ead7c239d2bc827874670ae76020c39bea5de
Your Name <you@example.com>
2024-07-08 19:07:46 +0530

    updated main file

--- app.py
+++ app.py
@@ -10,5 +10,6 @@ def index():
 def about():
     return render_template('about.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+
